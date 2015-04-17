1653d63fb29d7d998ea6e6382d196dd2462c80be
Enoch Shin <seekpeace@gmail.com>
2015-04-17 10:03:12 -0400

    httparty

--- devise_and_api/app/controllers/readers_controller.rb
+++ devise_and_api/app/controllers/readers_controller.rb
@@ -2,8 +2,17 @@
 
 class ReaderController < ApplicationController
 
-  def index
-    @items = Item.all
-  end
+def self(params[:id])
+  @items = Item.all
+  secret_key =ENV['apikey']
+
+  @response = HTTParty.get("https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/?text=#{itemlabel}@api_key=#{secret_key}",
+  headers:{
+      'X-Mashape-Key: yDhcmd1E2Amsh1OOrkYcDisKknyVp1Bnj4ujsnm4NYjxjJ5M7a',
+      'Accept: application/json'
+    }).parsed_response
+  #how to "speak" response??!!
+
+end
 
 end
--- devise_and_api/app/models/reader.rb
+++ devise_and_api/app/models/reader.rb
@@ -1,14 +1,6 @@
 class Reader
 
-  def self(params[:id])
-    @response = HTTParty.get("https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/?text=#{itemlabel}",
-    headers:{
-        'X-Mashape-Key: yDhcmd1E2Amsh1OOrkYcDisKknyVp1Bnj4ujsnm4NYjxjJ5M7a',
-        'Accept: application/json'
-      }).parsed_response
-    #how to "speak" response??!!
 
-  end
 # curl --get --include 'https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/?text= \
 #   -H 'X-Mashape-Key: yDhcmd1E2Amsh1OOrkYcDisKknyVp1Bnj4ujsnm4NYjxjJ5M7a' \
 #   -H 'Accept: application/json'
